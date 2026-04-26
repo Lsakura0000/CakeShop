84f1ce2a062410ecd93c92dec0c3ee1638a8c48f
Lsakura0000 <111075673+Lsakura0000@users.noreply.github.com>
2026-04-26 15:15:31 +0800

    Add files via upload

--- CakeShop/Program.cs
+++ CakeShop/Program.cs
@@ -2,47 +2,35 @@ using CakeShop.Data;
 using CakeShop.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. 設定 SQLite
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(option  => option.UseSqlite(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(option  => option.SignIn.RequireConfirmedAccount =  true);
+// 2. 設定 Identity (設定 RequireConfirmedAccount = true 強制信件驗證)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
+// 3. 註冊自定義的 Email 服務
 builder.Services.AddTransient<IEmailSender, EmailSender>();
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<ApplicationDbContext>();
-        context.Database.Migrate();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "資料遷移發生錯誤");
-    }
-}
-
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthentication(); // 啟用身分驗證
+app.UseAuthorization();  // 啟用授權
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller = Home}/{ Action = Index}/{ Id?}");
-app.MapRazorPages();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages(); // Identity UI 需要
 
 app.Run();
